ad219ffb634f5520b994eeab1c3fcd74d3b8bd97
gilbert-liang <gilbert.liang@cnosdb.com>
2022-03-17 15:05:59 +0800

    range&rangeInclusive

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-use num::complex::Complex;
-
-
+// 填空
+use std::ops::{Range, RangeInclusive};
 fn main() {
-    let a = Complex { re: 2.1, im: -1.2 };
-    let b = Complex::new(11.1, 22.2);
-    let result = a + b;
+    let r1 = Range{ start: 1, end: 6 };
+    let r2 = RangeInclusive::new(1, 5);
 
-    println!("{} + {}i", result.re, result.im)
+    assert_eq!((1..6), r1);
+    assert_eq!((1..=5),r2);
+    // assert_eq!(r1,r2);
+    // assert_eq!((1..6),(1..=5));
+    for i in r1 {
+        println!("{}",i);
+    }    
+    for i in r2 {
+        println!("{}",i);
+    }
 }
-  
