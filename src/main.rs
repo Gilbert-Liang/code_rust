f44538bfd089ea19161521affe7f7f532418df5c
gilbert-liang <gilbert.liang@cnosdb.com>
2022-03-17 11:13:04 +0800

    2.2

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 fn main() {
-    let (a, mut b):(bool,bool) = (true,false);
-    println!("a = {:?}, b = {:?}",a,b);
+    let sum = 5 + 10;
 
-    b = true;
-    assert_eq!(a,b);
+    // 减法
+    let difference = 95.5 - 4.3;
+
+    // 乘法
+    let product = 4 * 30;
+
+    // 除法
+    let quotient = 56.7 / 32.2;
+
+    // 求余
+    let remainder = 43 % 5;
+    println!("{} {} {} {} {}",sum,difference,product,quotient,remainder)
 }
