a413b42fc7b4f578764f4c42c25b2d0975c181b9
gilbert-liang <gilbert.liang@cnosdb.com>
2022-03-17 14:49:52 +0800

    complex

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use num::complex::Complex;
+
+
 fn main() {
-    for i in 1..5 {
-        println!("{}",i);
-    }
-    for i in 'a'..='z' {
-        println!("{}",i);
-    }    
+    let a = Complex { re: 2.1, im: -1.2 };
+    let b = Complex::new(11.1, 22.2);
+    let result = a + b;
+
+    println!("{} + {}i", result.re, result.im)
 }
   
