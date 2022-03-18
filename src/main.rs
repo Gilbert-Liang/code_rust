2ad410a87cf772773466f7617890c771c9364d1d
gilbert-liang <gilbert.liang@cnosdb.com>
2022-03-18 11:24:05 +0800

    for ever

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,22 @@
-// 填空，并解决错误
+#![allow(unused)]
+use std::fmt::Debug;
 fn main() {
-    // 整数加法
-    assert!(1u32 + 2 == 3);
+    let mut a = String::from("test");
+    clear(&mut a);
+    // dead_end();
+    forever();
+}
 
-    // 整数减法
-    assert!(1i32 - 2 == -1);
-    assert!(1u8 - 1 == 0);
-    
-    assert!(3 * 50 == 150);
+fn clear(text: &mut String) -> () {
+    *text = String::from("");
+}
 
-    assert!((9.6_f64 / 3.2 - 3.0).abs() < 0.1); // error ! 修改它让代码工作
+fn dead_end() -> ! {
+    panic!("你已经到了穷途末路，崩溃吧！");
+  }
 
-    assert!(24 % 5 == 4);
-    
-    // 逻辑与或非操作
-    assert!(true && false == false);
-    assert!(true || false == true);
-    assert!(!true ==false);
-
-    // 位操作
-    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
-    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
-    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
-    println!("1 << 5 is {}", 1u32 << 5);
-    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+fn forever() -> ! {
+    loop {
+      //...
+    };
 }
