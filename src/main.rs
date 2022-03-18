d5ee764f93697b06daec52906cd10829ea93182b
gilbert-liang <gilbert.liang@cnosdb.com>
2022-03-18 16:22:26 +0800

    5.1.2

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
-
+// 不要修改 main 中的代码
 fn main() {
-    // 使用尽可能多的方法来通过编译
-    // let x = String::from("hello, world");
-    // let y = x.clone();
+    let s1 = String::from("hello, world");
+    let s2 = take_ownership(s1);
 
-    // let x = "hello, world";
-    // let y = x;
+    println!("{}", s2);
+}
 
-    // let x = &String::from("hello,world");
-    // let y = x;
-
-    let x = 10;
-    let y = x;
-
-    println!("{},{}",x,y);
+// 只能修改下面的代码!
+fn take_ownership(s: String) -> String{
+    println!("{}", s);
+    s
 }
