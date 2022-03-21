97a2b91818bc897bb859528f67c81a9d883211ca
gilbert-liang <gilbert.liang@cnosdb.com>
2022-03-21 18:07:55 +0800

    Dangling References

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,8 @@
-// use std::thread;
 fn main() {
-    let mut s = String::from("hello");
-
-    change1(&mut s);
-    // thread::spawn(change1);
-    change2(&mut s);
-    println!("{}",s);
+    let reference_to_nothing = dangle();
 }
 
-fn change1(some_string:  &mut String) {
-    some_string.push_str(", world");
-}
-
-fn change2(some_string:  &mut String) {
-    some_string.push_str(", liangdong");
+fn dangle() -> &String {
+    let s = String::from("hello");
+    &s
 }
