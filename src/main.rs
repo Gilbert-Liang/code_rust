7f518e27016bff143dcbae166fa79ebababb107e
gilbert-liang <gilbert.liang@cnosdb.com>
2022-03-22 14:39:54 +0800

    two mut &?

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,12 @@
 
-// // 移除代码某个部分，让它工作
-// // 你不能移除整行的代码！
-// fn main() {
-//     let mut s = String::from("hello");
-
-//     let r1 = &s;
-//     let r2 = &s;
-
-//     println!("{}, {}", r1, r2);
-// }
-
-
-// 下面的代码没有任何错误
+// 注释掉一行代码让它工作
 fn main() {
     let mut s = String::from("hello, ");
 
-    borrow_object(&s);
+    let r1 = &mut s;
+    r1.push_str("world");
+    let r2 = &mut s;
+    r2.push_str("!");
     
-    s.push_str("world");
+    // println!("{}",s);
 }
-
-fn borrow_object(s: &String) {}
