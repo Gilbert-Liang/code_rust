24b989d30b8b2cc8fb914ddb2e62f9459f928c96
gilbert-liang <gilbert.liang@cnosdb.com>
2022-03-22 16:45:18 +0800

    chinese char

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,6 @@
+#![allow(unused)]
 fn main() {
-    let str1 = "Pascal";//堆内存
-    let str2 = "haskell";//堆内存
-    let s = String::from("hello world");
-
-    let hello = &s[0..5];
-    let world = &s[6..11];
-
-    println!("{:p}",str1);
-    println!("{:p}",str2);
-    println!("{:p}",s.as_ptr());
-    println!("{:p}",hello);
-    println!("{:p}",world);
-
-    // println!("{:p}",func1);
-    // println!("{:p}",func2);
-
-}
-
-fn func1() {
-
-}
-
-fn func2() {
-
+    let s = "中国人";
+    let a = &s[0..3];
+    println!("{}",a);
 }
