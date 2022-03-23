78285ab8201caf9e3d60c2f8c8e015bdf6bc371f
gilbert-liang <gilbert.liang@cnosdb.com>
2022-03-23 10:44:57 +0800

    chars&&bytes

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,17 @@
 fn main() {
-    // 创建一个空String
-    let mut s = String::new();
-    // 将&str类型的"hello,world"添加到s中
-    s.push_str("hello,world");
-    // 将字符'!'推入s中
-    s.push('!');
-    // 最后s的内容是"hello,world!"
-    assert_eq!(s,"hello,world!");
-
-    // 从现有的&str切片创建String类型
-    let mut s = "hello,world".to_string();
-    // 将字符'!'推入s中
-    s.push('!');
-    // 最后s的内容是"hello,world!"
-    assert_eq!(s,"hello,world!");
-
-    // 从现有的&str切片创建String类型
-    // String与&str都是UTF-8编码，因此支持中文
-    let mut s = String::from("你好,世界");
-    // 将字符'!'推入s中
-    s.push('!');
-    // 最后s的内容是"你好,世界!"
-    assert_eq!(s,"你好,世界!");
-
-    let s1 = String::from("hello,");
-    let s2 = String::from("world!");
-    // 在下句中，s1的所有权被转移走了，因此后面不能再使用s1
-    let s3 = s1 + &s2;
-    assert_eq!(s3,"hello,world!");
-    // 下面的语句如果去掉注释，就会报错
-    // println!("{}",s3);
-
-    let s1 = String::from("tic");
-    let s2 = String::from("tac");
-    let s3 = String::from("toe");
-
-    let s = s1 + "_" + &s2 + "_" + &s3;
-
-    println!("{}",s);
+    // for c in "中国人".chars() {
+    //     println!("{}",c);
+    // }
+    // for b in "中国人".bytes() {
+    //     println!("{}",b);
+    // }
+    let test = String::from("holla中国人नमस्ते");
+    for c in test.chars() {
+        println!("{}",c);
+    }
 }
 
-// fn add(self, s: &str) -> String {}
+
 
 
 
