f60623b4fb396471391e38b835a59e7f5ea5a501
gilbert-liang <gilbert.liang@cnosdb.com>
2022-03-23 11:41:31 +0800

    Box::str

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,8 @@
 fn main() {
-    // for c in "中国人".chars() {
-    //     println!("{}",c);
-    // }
-    // for b in "中国人".bytes() {
-    //     println!("{}",b);
-    // }
-    let test = String::from("holla中国人नमस्ते");
-    for c in test.chars() {
-        println!("{}",c);
-    }
-}
-
-
-
-
-
+    let s: Box<str> =  "hello, world".into();
+    greetings(&s)
+ }
+ 
+ fn greetings(s: &str) {
+     println!("{}",s)
+ }
