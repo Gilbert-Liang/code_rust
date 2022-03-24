7982afecdd855e44ace2b21abdadc3497becca75
gilbert-liang <gilbert.liang@cnosdb.com>
2022-03-24 11:37:00 +0800

    struct move/copy trait

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,61 @@
-// 6.3.1
-// 修复代码中的错误，不要新增代码行!
-// 一个切片是16个字节的大小
-
+// 2.4.2
 // fn main() {
-//     let arr = [1, 2, 3];
-//     let s1: &[i32] = &arr[0..2];
+//     let tup = (500, 6.4, 1);
 
-//     let s2: &str = "hello, world";
+//     // let (x, y, z) = tup;
+//     let firve_hundred = tup.0;
+//     let six_point_four = tup.1;
 
-//     println!("{:?} {:?} {:?}",arr,s1,s2);
+//     let one = tup.2;
+
+
+//     println!("The value of y is: {} {} {}", firve_hundred,six_point_four,one);
 // }
 
-// 6.3.2
-// use std::mem;
 // fn main() {
-//     let arr: [char; 5] = ['中', '国', '人', '可', '能'];
-//     let arr2: [char; 2] = ['中', '国'];
-//     let slice = &arr[..2];
-    
-//     // 修改数字 `6` 让代码工作
-//     // 小提示: 切片和数组不一样，它是引用。如果是数组的话，那下面的 `assert!` 将会通过： 因为'中'和'国'是 UTF-8 字符，它们每个占用 3 个字节，2 个字符就是 6 个字节
-//     assert!(std::mem::size_of_val(&slice) == 16);
-//     println!("{} {} {} {}",std::mem::size_of_val(&slice), std::mem::size_of_val(slice), std::mem::size_of_val(&arr), std::mem::size_of_val(&arr2));
+//     let s1 = String::from("hello");
+
+//     let (s2,len) = calculate_length(s1);
+
+//     println!("The length of '{}' is {}",s2,len);
 // }
 
-// 6.3.3
+// fn calculate_length(s: String) -> (String,usize) {
+//     let length = s.len();
 
-// fn main() {
-//     let arr: [i32; 5] = [1, 2, 3, 4, 5];
-//     // 填空让代码工作起来
-//     let slice: &[i32] = &arr[1..4];
-//     assert_eq!(slice, &[2, 3, 4]);
-//     println!("{:?}",slice);
+//     (s, length)
 // }
 
-// 6.3.4
-// fn main() {
-//     let s = String::from("hello");
-
-//     let slice1 = &s[0..2];
-//     // 填空，不要再使用 0..2
-//     let slice2 = &s[..2];
-
-//     assert_eq!(slice1, slice2);
-// }
-// 6.3.5
-
-// fn main() {
-//     let s = "你好，世界";
-//     // 修改以下代码行，让代码工作起来
-//     let slice = &s[0..3];
-
-//     assert!(slice == "你");
-// }
-// 6.3.6
-
-// 修复所有错误
+#![allow(unused)]
+#[derive(Debug)]
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
 fn main() {
-    let mut s = String::from("hello world");
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+    let user2 = User {
+        email:String::from("another@example.com"),
+        username:String::from("someusername234"),
+        active:user1.active,
+        sign_in_count:user1.sign_in_count,
+    };
+    let mut user3 = User {
+        email: String::from("another1@example.com"),
+        ..user1
+    };
 
-    // 这里, &s 是 `&String` 类型，但是 `first_word` 函数需要的是 `&str` 类型。
-    // 尽管两个类型不一样，但是代码仍然可以工作，原因是 `&String` 会被隐式地转换成 `&str` 类型，如果大家想要知道更多，可以看看 Deref 章节: https://course.rs/advance/smart-pointer/deref.html
-    let word = first_word(&s);
-
-    println!("the first word is: {}", word);
-
-    s.clear(); // error!
-
-    
+    user3.username = String::from("last");
+    println!("{:?}",user1.email);
+    println!("{:?}",user2);
+    println!("{:?}",user3);
 }
-fn first_word(s: &str) -> &str {
-    &s[..1]
-}
 
-  
 
