42149c7f4369a7e6ac4d4500b636f8a5ec76cfe4
gilbert-liang <gilbert.liang@cnosdb.com>
2022-03-25 13:27:44 +0800

    enum vs pao

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
-enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(i32, i32, i32),
-}
-//不要滥用枚举
+// enum Message {
+//     Quit,
+//     Move { x: i32, y: i32 },
+//     Write(String),
+//     ChangeColor(i32, i32, i32),
+// }
+// //不要滥用枚举
+// fn main() {
+//     let m1 = Message::Quit;
+//     let m2 = Message::Move{x:1,y:1};
+//     let m3 = Message::ChangeColor(255,255,0);
+// }
+
+#![allow(unused)]
+
 fn main() {
-    let m1 = Message::Quit;
-    let m2 = Message::Move{x:1,y:1};
-    let m3 = Message::ChangeColor(255,255,0);
+
+}
+
+fn new (stream: TcpStream) {
+    let mut s = stream;
+    if tls {
+      s = negotiate_tls(stream)
+    }
+  
+    // websocket是一个WebSocket<TcpStream>或者
+    //   WebSocket<native_tls::TlsStream<TcpStream>>类型
+    websocket = WebSocket::from_raw_socket(
+      stream, ......)
+}
+
+enum Websocket {
+    Tcp(Websocket<TcpStream>),
+    TLs(Websocket<native_tls::TlsStream>;)
 }
