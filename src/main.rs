bb6ad4083aab0da7e26dc366da3404e34d72ea27
gilbert-liang <gilbert.liang@cnosdb.com>
2022-03-25 14:08:02 +0800

    add for Option<T;2C>

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,19 @@
-// enum Message {
-//     Quit,
-//     Move { x: i32, y: i32 },
-//     Write(String),
-//     ChangeColor(i32, i32, i32),
-// }
-// //不要滥用枚举
-// fn main() {
-//     let m1 = Message::Quit;
-//     let m2 = Message::Move{x:1,y:1};
-//     let m3 = Message::ChangeColor(255,255,0);
-// }
-
-#![allow(unused)]
-
 fn main() {
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
 
-}
+    // let sum = x + y;
 
-fn new (stream: TcpStream) {
-    let mut s = stream;
-    if tls {
-      s = negotiate_tls(stream)
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+} 
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i+1),
     }
-  
-    // websocket是一个WebSocket<TcpStream>或者
-    //   WebSocket<native_tls::TlsStream<TcpStream>>类型
-    websocket = WebSocket::from_raw_socket(
-      stream, ......)
 }
 
-enum Websocket {
-    Tcp(Websocket<TcpStream>),
-    TLs(Websocket<native_tls::TlsStream>;)
-}
