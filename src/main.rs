18874c986b6c5a34a641e643306a8dde06182b8b
gilbert-liang <gilbert.liang@cnosdb.com>
2022-03-25 13:16:37 +0800

    enum message

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
-struct Ipv4Addr {
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }
-
-struct Ipv6Addr {
-}
-
-enum IpAddr {
-    V4(Ipv4Addr),
-    V6(Ipv6Addr),
-}
-
+//不要滥用枚举
 fn main() {
-    
+    let m1 = Message::Quit;
+    let m2 = Message::Move{x:1,y:1};
+    let m3 = Message::ChangeColor(255,255,0);
 }
