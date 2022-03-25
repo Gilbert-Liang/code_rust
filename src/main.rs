ccb10ebf0cf0a3217ddef1889bcf2e4d29b4974a
gilbert-liang <gilbert.liang@cnosdb.com>
2022-03-25 11:57:09 +0800

    enum2

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,14 @@
-#[derive(Debug)]
-enum PokerCard {
-    Clubs(u8),
-    Spades(u8),
-    Diamonds(char),
-    Hearts(char),
+struct Ipv4Addr {
+}
+
+struct Ipv6Addr {
+}
+
+enum IpAddr {
+    V4(Ipv4Addr),
+    V6(Ipv6Addr),
 }
 
 fn main() {
-    // let c1 = PokerCard {
-    //     suit: PokerSuit::Clubs,
-    //     value: 1,
-    // };
-    // let c2 = PokerCard {
-    //     suit: PokerSuit::Diamonds,
-    //     value: 12,
-    // };
-    let c1 = PokerCard::Spades(5);
-    let c2 = PokerCard::Diamonds('A');
-
-    println!("{:?}",c1);
-    println!("{:?}",c2);
+    
 }
