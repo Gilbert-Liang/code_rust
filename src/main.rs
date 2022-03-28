1e07854017d49a2357748968858f25425a3c5e82
gilbert-liang <gilbert.liang@cnosdb.com>
2022-03-28 20:48:37 +0800

    plus_one

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,13 @@
-// fn main() {
-//     let age = Some(30);
-//     println!("在匹配前，age是{:?}",age);
-//     if let Some(age) = age {
-//         println!("匹配出来的age是{}",age);
-//     }
- 
-//     println!("在匹配后，age是{:?}",age);
-//  }
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
 
 fn main() {
-    let age = Some(30);
-    println!("在匹配前，age是{:?}",age);
-    match age {
-        Some(age) =>  println!("匹配出来的age是{}",age),
-        _ => ()
-    }
-    println!("在匹配后，age是{:?}",age);
- }
- 
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+}
