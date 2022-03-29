202dc07121d04c07a39b54a2d520f69f17e617e1
gilbert-liang <gilbert.liang@cnosdb.com>
2022-03-29 18:12:46 +0800

    2.6

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,29 @@
-// fn main() {
-//     let s = Some(String::from("Hello!"));
-
-//     if let Some(_) = s {
-//         println!("found a string");
-//     }
-//     println!("{:?}", s);
+// #[derive(Debug)]
+// struct Point {
+//     x: i32,
+//     y: i32,
 // }
 
 // fn main() {
-//     let numbers = (2, 4, 8, 16, 32);
+//     // 绑定新变量 `p`，同时对 `Point` 进行解构
+//     let p @ Point {x: px, y: py } = Point {x: 10, y: 23};
+//     println!("x: {}, y: {}", px, py);
+//     println!("{:?}", p);
 
-//     match numbers {
-//         (.., second, ..) => {
-//             println!("Some numbers: {}", second)
-//         },
+
+//     let point = Point {x: 10, y: 5};
+//     if let p @ Point {x: 10, y} = point {
+//         println!("x is 10 and y is {} in {:?}", y, p);
+//     } else {
+//         println!("x was not 10 :(");
 //     }
 // }
-
-#![allow(unused)]
-// fn main() {
-//     let num = Some(4);
-
-//     match num {
-//         Some(x) if x < 5 => println!("less than five: {}", x),
-//         Some(x) => println!("{}", x),
-//         None => (),
-//     }
-// }
-
-enum Message {
-    Hello { id: i32 },
-}
-
-
-
 
 fn main() {
-
-    let msg = Message::Hello { id: 1 };
-
-    match msg {
-        Message::Hello { id: id_variable @ 3..=7 } => {
-            println!("Found an id in range: {}", id_variable)
-        },
-        Message::Hello { id: 10..=12 } => {
-            println!("Found an id in another range")
-        },
-        Message::Hello { id } => {
-            println!("Found some other id: {}", id)
-        },
+    match 1 {
+        num @ (1 | 2) => {
+            println!("{}", num);
+        }
+        _ => {}
     }
-    // let x = Some(5);
-    // let y = 10;
-
-    // match x {
-    //     Some(50) => println!("Got 50"),
-    //     Some(n) if n == y => println!("Matched, n = {}", n),
-    //     _ => println!("Default case, x = {:?}", x),
-    // }
-
-    // println!("at the end: x = {:?}, y = {}", x, y);
-    // let x = 4;
-    // let y = false;
-
-    // match x {
-    //     4 | 5 | 6 if y => println!("yes"),
-    //     _ => println!("no"),
-    // }
 }
