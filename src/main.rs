b42b8ab3bb8472270e7bcaef3eed54cda905b794
gilbert-liang <gilbert.liang@cnosdb.com>
2022-03-30 11:56:40 +0800

    method

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,69 @@
-// #[derive(Debug)]
-// struct Point {
-//     x: i32,
-//     y: i32,
-// }
+#![allow(unused)]
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
 
-// fn main() {
-//     // 绑定新变量 `p`，同时对 `Point` 进行解构
-//     let p @ Point {x: px, y: py } = Point {x: 10, y: 23};
-//     println!("x: {}, y: {}", px, py);
-//     println!("{:?}", p);
+impl Message {
+    fn call(&self) {
+        // 在这里定义方法体
+    }
+}
 
+struct Circle {
+    x: f64,
+    y: f64,
+    radius: f64,
+}
 
-//     let point = Point {x: 10, y: 5};
-//     if let p @ Point {x: 10, y} = point {
-//         println!("x is 10 and y is {} in {:?}", y, p);
-//     } else {
-//         println!("x was not 10 :(");
-//     }
-// }
+impl Circle {
+    // new是Circle的关联函数，因为它的第一个参数不是self
+    // 这种方法往往用于初始化当前结构体的实例
+    fn new(x: f64, y: f64, radius: f64) -> Circle {
+        Circle {
+            x: x,
+            y: y,
+            radius: radius,
+        }
+    }
+
+    // Circle的方法，&self表示借用当前的Circle结构体
+    fn area(&self) -> f64 {
+        std::f64::consts::PI * (self.radius * self.radius)
+    }
+}
+
+pub struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    pub fn new(width: u32, height: u32) -> Self {
+        Rectangle { width, height }
+    }
+    pub fn width(&self) -> u32 {
+        return self.width;
+    }
+    pub fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+impl Rectangle {
+    pub fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
 
 fn main() {
-    match 1 {
-        num @ (1 | 2) => {
-            println!("{}", num);
-        }
-        _ => {}
-    }
+    let rect1 = Rectangle::new(30, 50);
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!("{}", rect1.width());
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
